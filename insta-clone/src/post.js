2ad5b3f1599182d05c410f62d785d1b92a09a2ae
mshowkat <md.showkat8@gmail.com>
2020-09-11 23:33:47 +0600

    header done

---
+++ insta-clone/src/post.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function post() {
+    return (
+        <div>
+            <div className="post__header">
+
+            </div>
+        </div>
+    )
+}
+
+export default post

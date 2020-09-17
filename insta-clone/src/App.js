c68bc23475479cbbbaeec9ebc30363146676fb86
mshowkat <md.showkat8@gmail.com>
2020-09-17 20:55:11 +0600

    state setup

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo192.png';
 import logo1 from './logo.svg';
 import './App.css';
 import Post from './Post'
 
 function App() {
-  // const [post, setpost] = useState(initialState)
+  const [posts, setpost] = useState([
+    {
+      userName: "JustShowkat",
+      caption: "new day to learn react",
+      imageUrl: logo1,
+    },
+    {
+      userName: "JustShowkat",
+      caption: "new day to learn react",
+      imageUrl: logo
+    },
+  ])
   
   return (
     <div className="App">
@@ -13,11 +24,16 @@ function App() {
         <img className="app__headerImage" src={logo} alt="logo icon"/>
       </div>
       <div>
-        <Post userName="JustShowkat" caption="new day to learn react" imageUrl={logo1} />
+        {
+          posts.map(post => (
+            <Post userName={post.userName} caption={post.caption} imageUrl={post.imageUrl} />
+          ))
+        }
+        {/* <Post userName="JustShowkat" caption="new day to learn react" imageUrl={logo1} />
         <Post userName="ima" caption="new day to learn react" imageUrl={logo} />
         <Post userName="choyon" caption="new day to learn react" imageUrl="https://www.pexels.com/photo/black-and-silver-dslr-camera-on-white-table-4031005/" />
         <Post userName="alisha" caption="new day to learn react" imageUrl="image" />
-        <Post userName="afra" caption="new day to learn react" imageUrl="image" />
+        <Post userName="afra" caption="new day to learn react" imageUrl="image" /> */}
       </div>
     </div>
   );

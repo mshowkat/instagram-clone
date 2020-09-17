498c92d7cbcd334dcc871056615ea0660cb97dd7
mshowkat <md.showkat8@gmail.com>
2020-09-17 20:41:50 +0600

    card ready

--- insta-clone/src/App.js
+++ insta-clone/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo192.png';
+import logo1 from './logo.svg';
 import './App.css';
 import Post from './Post'
 
 function App() {
+  // const [post, setpost] = useState(initialState)
+  
   return (
     <div className="App">
       <div className="app__header">
         <img className="app__headerImage" src={logo} alt="logo icon"/>
       </div>
       <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post userName="JustShowkat" caption="new day to learn react" imageUrl={logo1} />
+        <Post userName="ima" caption="new day to learn react" imageUrl={logo} />
+        <Post userName="choyon" caption="new day to learn react" imageUrl="https://www.pexels.com/photo/black-and-silver-dslr-camera-on-white-table-4031005/" />
+        <Post userName="alisha" caption="new day to learn react" imageUrl="image" />
+        <Post userName="afra" caption="new day to learn react" imageUrl="image" />
       </div>
     </div>
   );
--- insta-clone/src/Post.js
+++ insta-clone/src/Post.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './Post.css';
 import avatar from "./avatar.png"
 import Avatar from '@material-ui/core/Avatar';
 
 
-function post() {
+function post({userName, caption, imageUrl}) {
     return (
         <div className="post">
             <div className="post__header">
                 <Avatar className='post__avatar' alt="M showkat" src={avatar} />
-                <h3>Just Showkat</h3>
+                <h3>{userName}</h3>
             </div>
-            <img className="post__image" src={logo} alt="avatar"/>            
-            <h4 className="post__text"><strong>Mohammad Showkat: </strong> new day to learn react developement</h4>
+            <img className="post__image" src={imageUrl} alt="post image..."/>            
+            <h4 className="post__text"><strong>{userName}: </strong> {caption}</h4>
         </div>
     )
 }
